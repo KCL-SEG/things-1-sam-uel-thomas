fc28f5f9e95a541b2e92d8b48d39fa0c307a9963
Sam Thomas <samuel.mmthomas@gmail.com>
2023-12-08 11:52:44 +0000

    Rename model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class things(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(blank=True, max_length=120, unique = False)
     quantity = models.IntegerField(
